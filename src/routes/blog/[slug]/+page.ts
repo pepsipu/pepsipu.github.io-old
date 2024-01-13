b98cd4773f450927436fa4dfb3e747ee04787693
Sammy Hajhamid <sh@pepsi.pw>
2024-01-12 22:42:57 -0800

    sveltemaxxing + don't care + hidden urls now work

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,9 +1,16 @@
-import { getPostBySlug } from "$lib/post";
+import { getPostBySlug, posts } from "$lib/post";
+import type { EntryGenerator } from "./$types";
 
 export const load = async ({ params }) => {
   return {
     post: getPostBySlug(params.slug),
   };
+};
+
+export const entries: EntryGenerator = () => {
+  return posts.map(({ slug }) => ({
+    slug,
+  }));
 };
 
 export const prerender = true;
